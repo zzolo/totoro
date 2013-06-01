186e3f30443e7cc188c2ca779e9860ce6584aad0
Alan Palazzolo <apalazzolo@minnpost.com>
2013-06-01 15:25:25 -0500

    Adding in map of stops.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Bus Stops
 
 Bus stop visualizations
+
+## Data
+
+* Stop data from [here](https://github.com/r-barnes/mspbus/blob/master/data/stops.txt) via MetroTransit GTFS feed.
+
+## Install
+
+1. Install `bower`
+1. Run: `bower install`
--- js/app.js
+++ js/app.js
@@ -3,15 +3,37 @@
  */
 
 (function($, undefined) {
+  var busStop = '17984';
   
   $(document).ready(function() {
     
-    $.getJSON('http://svc.metrotransit.org/NexTrip/85?format=json&callback=?', function(data) {
+    $.getJSON('./js/stops.json', function(stops) {
+      $.getJSON('http://svc.metrotransit.org/NexTrip/' + busStop + '?format=json&callback=?', function(currentStop) {
+        
+        //console.log(currentStop);
+        //$('.bus-coming').html('Bus ' + currentStop[0].Route + ' is coming in ' + currentStop[0].DepartureText);
+        
+        // Create map
+        var map = L.map('bus-stop-map')
+          .setView([stops[busStop].lat, stops[busStop].lon], 12);
+        
+        // add an OpenStreetMap tile layer
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+        
+        // Add bus stop marker
+        L.marker([stops[busStop].lat, stops[busStop].lon]).addTo(map);
+        
+        // Add bus marker
+        for (var bus in currentStop) {
+          if (currentStop[bus].Actual == true) {
+            L.marker([currentStop[bus].VehicleLatitude, currentStop[bus].VehicleLongitude]).addTo(map)
+              .bindPopup('Bus ' + currentStop[bus].Route + currentStop[bus].Terminal + ' is arriving in approximately ' + currentStop[bus].DepartureText);
+          }
+        }
       
-      console.log(data);
-      
-      $('.bus-coming').html('Bus ' + data[0].Route + ' is coming in ' + data[0].DepartureText);
-    
+      });
     });
   });
   
